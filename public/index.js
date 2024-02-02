92d7d98c8c8b058d019c4b5233996e9344b2f2ed
Louiza Mak <louiza.mak@gmail.com>
2024-02-02 16:19:50 -0600

    feat: create basic form for GET request params

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     fetchData();
+    buildForm();
 });
 
 function fetchData() {
@@ -8,3 +9,20 @@ function fetchData() {
     .then(res => res.json())
     .then(res => console.log(res));
 }
+
+function handleForm() {
+    let form = document.querySelector('form');
+    let paramsObj = {
+        zipCode: null,
+        firstName: null,
+        lastName: null,  
+    }
+    form.addEventListener('submit', event => {
+        event.preventDefault()
+        paramsObj.zipCode = event.target.zipCode.value;
+        paramsObj.firstName = event.target.firstName.value;
+        paramsObj.lastName = event.target.lastName.value;
+        console.log(paramsObj);
+    })
+}
+

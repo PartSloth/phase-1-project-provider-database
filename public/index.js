e9176d637161a236c6e27b46035be2ace767a37f
Louiza Mak <louiza.mak@gmail.com>
2024-02-02 19:29:55 -0600

    Add functions to extract and format form data for GET requests

--- public/index.js
+++ public/index.js
@@ -1,28 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetchData();
-    buildForm();
+    handleForm();
 });
-
-function fetchData() {
-    let queryParams = "&first_name=Mary"
-    fetch(`/api/${queryParams}`)
-    .then(res => res.json())
-    .then(res => console.log(res));
-}
 
 function handleForm() {
     let form = document.querySelector('form');
     let paramsObj = {
-        zipCode: null,
-        firstName: null,
-        lastName: null,  
+        postal_code: null,
+        first_name: null,
+        last_name: null,  
     }
     form.addEventListener('submit', event => {
+        let queryParams;
         event.preventDefault()
-        paramsObj.zipCode = event.target.zipCode.value;
-        paramsObj.firstName = event.target.firstName.value;
-        paramsObj.lastName = event.target.lastName.value;
-        console.log(paramsObj);
+        paramsObj.postal_code = event.target.postal_code.value;
+        paramsObj.first_name = event.target.first_name.value;
+        paramsObj.last_name = event.target.last_name.value;
+        queryParams = createParams(paramsObj);
+        fetchData(queryParams);
     })
 }
 
+function createParams(paramsObj) {
+    let queryParams = "";
+    for (const param in paramsObj) {
+        if(paramsObj[param].length > 0) {
+            queryParams = queryParams + `&${param}=${paramsObj[param]}`
+        }
+    }
+    return queryParams;
+}
+
+function fetchData(queryParams) {
+    fetch(`/api/${queryParams}`)
+    .then(res => res.json())
+    .then(res => console.log(res));
+}
